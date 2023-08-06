6368379b1ca29bc548441fcff2efd5b6a04b7c88
Brian Mungai <bryanmungai254@gmail.com>
2023-08-06 15:59:34 +0300

    port situation

--- backend/main.go
+++ backend/main.go
@@ -74,12 +74,12 @@ func main() {
 	port := os.Getenv("PORT")
 
 	if port == ""{
-		port = ":8000"
+		port = "8000"
 	}
 
 	fmt.Println("Listening on port:", port)
 
-	err := app.Listen(port)
+	err := app.Listen(":" + port)
 
 	if err != nil {
 		panic(err)
